5527034e7b94f322c0eb22322c7773d0892ef0e8
Fernandolilo <fernando.sistenpro@outlook.com>
2022-04-06 07:25:11 -0300

    Iniciando o load balance e passando por paramento port

--- cambio-service/src/main/java/br/com/sistempro/cambioservice/CambioController.java
+++ cambio-service/src/main/java/br/com/sistempro/cambioservice/CambioController.java
@@ -2,6 +2,8 @@ package br.com.sistempro.cambioservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,21 @@ import br.com.sistempro.cambioservice.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
-
+	
+	/*
+	 * URL do endpoint
+	 * http://localhost:8000/cambio-service/5/USD/BRL
+	 * expor a porta com o environment
+	 */
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from,
 			@PathVariable("to") String to) {
-		return new Cambio(1L, "USD", "BRL", BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		//pergar a porta com o environment
+		var port = environment.getProperty("local.server.port");
+		return new Cambio(1L, "USD", "BRL", BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 
 }
