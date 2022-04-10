838685ceb3a2d259be4dd57f4b80a45e726dbcfb
Fernandolilo <fernando.sistenpro@hotmail.com>
2022-04-09 23:24:20 -0300

    Flywaydb-MySql - migration, versionando banco de dados

---
+++ cambio-service/src/main/java/br/com/sistempro/cambioservice/controller/CambioController.java
@@ -0,0 +1,40 @@
+package br.com.sistempro.cambioservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sistempro.cambioservice.model.Cambio;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+
+	/*
+	 * URL do endpoint http://localhost:8000/cambio-service/5/USD/BRL expor a porta
+	 * com o environment
+	 */
+
+	/*
+	 * para iniciar o load balance, no dashBoard, clicar com o direito, duplicate
+	 * App, open config, em arguents passar o paramentro -Dserver.port=8001 como por
+	 * padrão estou usando a porta 8000, sempre a mais proxima
+	 */
+
+	@Autowired
+	private Environment environment;
+
+	@GetMapping(value = "/{amount}/{from}/{to}")
+	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from,
+			@PathVariable("to") String to) {
+		// pergar a porta com o environment
+		var port = environment.getProperty("local.server.port");
+		return new Cambio(1L, "USD", "BRL", BigDecimal.ONE, BigDecimal.ONE, port);
+	}
+
+}
