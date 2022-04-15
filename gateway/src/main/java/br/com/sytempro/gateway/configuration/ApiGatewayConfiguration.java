f09e0093871d110230378da8a591c635b140b0ce
Fernandolilo <fernando.sistenpro@hotmail.com>
2022-04-15 09:40:28 -0300

    Passado paramentros para test da API-GETEWAY

--- gateway/src/main/java/br/com/sytempro/gateway/configuration/ApiGatewayConfiguration.java
+++ gateway/src/main/java/br/com/sytempro/gateway/configuration/ApiGatewayConfiguration.java
@@ -12,20 +12,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfiguration {
+	/*
+	 * "http://httpbin.orb:80" esta uri é padrão, é uma ferramenta de diagnostico
+	 * que converte chamadas http em um Json como resposta com o diagnostico de sua
+	 * api
+	 */
 
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		Function<PredicateSpec, Buildable<Route>>
 		function = p -> p.path("/get")
+						.filters(f -> f
+								.addRequestHeader("Hello", "Word")
+								.addRequestParameter("Hello", "Word"))
+						
 						.uri("http://httpbin.org:80");
 		return builder.routes()
 				.route(function)
 				.build();
-		/*
-		 * "http://httpbin.orb:80" esta uri é padrão, é uma ferramenta de diagnostico
-		 * que converte chamadas http em um Json como resposta com o diagnostico de sua
-		 * api
-		 */
+		
 
 		
 	}
