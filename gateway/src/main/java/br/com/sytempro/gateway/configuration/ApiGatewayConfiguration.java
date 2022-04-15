9e75fdbe2db17a1b1eeba80c883a29698594ef3d
Fernandolilo <fernando.sistenpro@hotmail.com>
2022-04-15 09:32:48 -0300

    Iniciado a roteirizacao com a API-GETEWAY

---
+++ gateway/src/main/java/br/com/sytempro/gateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.sytempro.gateway.configuration;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		Function<PredicateSpec, Buildable<Route>>
+		function = p -> p.path("/get")
+						.uri("http://httpbin.org:80");
+		return builder.routes()
+				.route(function)
+				.build();
+		/*
+		 * "http://httpbin.orb:80" esta uri é padrão, é uma ferramenta de diagnostico
+		 * que converte chamadas http em um Json como resposta com o diagnostico de sua
+		 * api
+		 */
+
+		
+	}
+
+}
