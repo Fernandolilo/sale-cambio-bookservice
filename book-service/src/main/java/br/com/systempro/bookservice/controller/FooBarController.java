58e282d018e50e06e01fb290eb482dbdf95b2ee5
fernandolilo <fernando.sistenpro@outlook.com>
2022-05-02 08:02:43 -0300

    Finalizado resilience4j

--- book-service/src/main/java/br/com/systempro/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/br/com/systempro/bookservice/controller/FooBarController.java
@@ -7,18 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
-	private Logger log = LoggerFactory.getLogger(FooBarController.class);
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("/foo-bar")
-	public String fooBar() {
-		
-		//new RestTemplate().getForEntity("http://localhost:8085/foo-bar", String.class);
+	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	public String foobar() {
+		logger.info("Request to foobar is receved!");
+		var response = new RestTemplate().getForEntity("http://localhost:8085/foo-bar", String.class);
+		// return "foo-bar!!!";
 
-		return "Foo-Bar!!!";
+		return response.getBody();
 	}
-
+	
+	public String fallbackMethod (Exception ex) {
+		return "fallbackMethod foo-bar";
+	}
 }
